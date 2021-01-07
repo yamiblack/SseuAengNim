3cece84b28338a08c4a2c8b175c524590035675f
jsb408 <jsb408@naver.com>
2021-01-07 17:13:07 +0900

    feat: Modify Constants

--- app/src/main/java/com/jaktongdan/android/sseuaengnim/Constants.kt
+++ app/src/main/java/com/jaktongdan/android/sseuaengnim/Constants.kt
@@ -4,12 +4,17 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+enum class Firestore {
+    MEMBER, BOARD, POST, COMMENT
+}
+
 enum class Preference(var id: String) {
     SETTINGS("settings")
 }
 
 enum class Settings(var id: String, var default: Any) {
-    AUTOLOGIN("auto_login", false)
+    AUTOLOGIN("auto_login", true),
+    NICKNAME("nickname", "")
 }
 
 //Google Firebase
