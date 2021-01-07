b704ed36b75dd74b4a7e8b454fc300dc1f3ab225
SubinJang <jsb408@gmail.com>
2021-01-07 11:07:00 +0900

    feat: Develop Join and Login Feature

--- app/src/main/java/com/jaktongdan/android/sseuaengnim/Constants.kt
+++ app/src/main/java/com/jaktongdan/android/sseuaengnim/Constants.kt
@@ -4,6 +4,14 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
+enum class Preference(var id: String) {
+    SETTINGS("settings")
+}
+
+enum class Settings(var id: String, var default: Any) {
+    AUTOLOGIN("auto_login", false)
+}
+
 //Google Firebase
 val kAuth = Firebase.auth
 val kFirestore = Firebase.firestore
--- app/src/main/java/com/jaktongdan/android/sseuaengnim/SplashActivity.kt
+++ app/src/main/java/com/jaktongdan/android/sseuaengnim/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.jaktongdan.android.sseuaengnim
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.os.Handler
 import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+    private val preference by lazy { getSharedPreferences(Preference.SETTINGS.id, Context.MODE_PRIVATE)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -14,7 +17,15 @@ class SplashActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+                startActivity(Intent(this,
+                        if(preference.getBoolean(Settings.AUTOLOGIN.id, Settings.AUTOLOGIN.default as Boolean)
+                                && kAuth.currentUser != null)
+                            MainActivity::class.java
+                        else {
+                            kAuth.signOut()
+                            LoginActivity::class.java
+                        }
+                ))
             finish()
         }, 1500)
     }
