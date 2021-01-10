c2f0ab1de0bfccb822f4f91722aded1ef87829fe
SubinJang <jsb408@gmail.com>
2021-01-10 18:28:18 +0900

    feat: Post, Comment thumbs

--- app/src/main/java/com/jaktongdan/android/sseuaengnim/PostDetailActivity.kt
+++ app/src/main/java/com/jaktongdan/android/sseuaengnim/PostDetailActivity.kt
@@ -1,13 +1,14 @@
 package com.jaktongdan.android.sseuaengnim
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.EditText
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
-import com.google.firebase.firestore.Query
 import com.jaktongdan.android.sseuaengnim.adapter.CommentRecyclerViewAdapter
 import com.jaktongdan.android.sseuaengnim.databinding.ActivityPostDetailBinding
 import com.jaktongdan.android.sseuaengnim.model.CommentData
@@ -26,13 +27,45 @@ class PostDetailActivity : AppCompatActivity() {
         val post = kFirestore.collection(Firestore.BOARD.name).document(boardId)
                 .collection(Firestore.POST.name).document(postId)
 
-        post.addSnapshotListener { value, _ ->
+        post.addSnapshotListener { value, e ->
+            e?.let {
+                e.printStackTrace()
+                return@addSnapshotListener
+            }
+
             supportActionBar?.title = value!!.getString("title")
 
             binding.apply {
                 textPostDetailTitle.text = value.getString("title")
                 textPostDetailContent.text = value.getString("content")
                 textPostDetailThumbs.text = (value["thumbs"] as List<*>? ?: listOf<String>()).size.toString()
+            }
+
+            if(value.getDocumentReference("writer")!!.id == kAuth.uid!!) {
+                binding.iconPostDetailThumbs.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_baseline_thumb_up_24))
+            } else {
+                var thumbsIcon = R.drawable.ic_outline_thumb_up_24
+                var clickListener = View.OnClickListener {
+                    value.reference.update(
+                            "thumbs", mutableListOf<String>().apply {
+                        addAll(value["thumbs"] as List<String>? ?: listOf())
+                        add(kAuth.uid!!)
+                    })
+                }
+
+                if ((value["thumbs"] as List<*>? ?: listOf<String>()).contains(kAuth.uid!!)) {
+                    thumbsIcon = R.drawable.ic_baseline_thumb_up_24
+                    clickListener = View.OnClickListener {
+                        value.reference.update(
+                                "thumbs", mutableListOf<String>().apply {
+                            addAll(value["thumbs"] as List<String>? ?: listOf())
+                            remove(kAuth.uid!!)
+                        })
+                    }
+                }
+
+                binding.iconPostDetailThumbs.setImageDrawable(ContextCompat.getDrawable(this, thumbsIcon))
+                binding.iconPostDetailThumbs.setOnClickListener(clickListener)
             }
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -41,7 +74,7 @@ class PostDetailActivity : AppCompatActivity() {
                 .whereEqualTo("post", post)
                 .orderBy("date")
         val options = FirestoreRecyclerOptions.Builder<CommentData>().setQuery(query, CommentData::class.java).build()
-        val commentRecyclerViewAdapter = CommentRecyclerViewAdapter(options)
+        val commentRecyclerViewAdapter = CommentRecyclerViewAdapter(options, binding)
 
         binding.recyclerViewPostDetailComment.adapter = commentRecyclerViewAdapter
         binding.recyclerViewPostDetailComment.layoutManager = object : LinearLayoutManager(this) {
@@ -63,6 +96,8 @@ class PostDetailActivity : AppCompatActivity() {
                         )
                 ).addOnSuccessListener {
                     binding.editTextPostDetailComment.text.clear()
+                    (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager)
+                            .hideSoftInputFromWindow(binding.editTextPostDetailComment.windowToken, 0)
                 }.addOnFailureListener { e ->
                     Toast.makeText(this, e.localizedMessage, Toast.LENGTH_LONG).show()
                 }
